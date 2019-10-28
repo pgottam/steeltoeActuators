e83b5b7b075d7de6f181c4816163c0509deefd80
PRAVEEN G <praveeng@PRAVEENs-MacBook-Pro-2.local>
2019-10-28 09:09:31 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Logging;
+
+namespace Actuators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseCloudFoundryHosting()
+                .AddCloudFoundry()
+                .ConfigureLogging((builderContext, loggingBuilder) =>
+                {
+                    loggingBuilder.AddConfiguration(builderContext.Configuration.GetSection("Logging"));
+                    loggingBuilder.AddDynamicConsole();
+                })
+                .UseStartup<Startup>()
+            .ConfigureAppConfiguration((context, builder) =>
+            {
+                var env = context.HostingEnvironment;
+                builder.SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                    // AddConfigServer adds AddCloudFoundry for us so we don't need to explicitly include it.
+                    // Since AddCloudFoundry is added, we also get the VCAP app & service configurations.
+                    //.AddConfigServer(env, new LoggerFactory().AddConsole(LogLevel.Information))
+                    .AddEnvironmentVariables();
+            });
+    }
+}
